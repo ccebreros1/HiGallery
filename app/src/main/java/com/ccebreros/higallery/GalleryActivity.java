89bd536288469623d3b6e86922a2e7ee233abb0e
ccebreros1 <cesar.cebreros10@gmail.com>
2017-04-05 16:43:01 -0600

    Tried to do external intent. Does not work

--- app/src/main/java/com/ccebreros/higallery/GalleryActivity.java
+++ app/src/main/java/com/ccebreros/higallery/GalleryActivity.java
@@ -1,15 +1,20 @@
 package com.ccebreros.higallery;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.content.FileProvider;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.GridView;
+import android.widget.Toast;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -24,10 +29,18 @@ public class GalleryActivity extends Activity {
     GridView gridView;
     Button newImageButton;
 
+    //For image handling
+    File folder;
+
+    Bitmap[] images;
+    Bitmap image;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.gallery);
+        ImageAdapter imageAdapter1 = new ImageAdapter(this);
+        images = imageAdapter1.getImages();
         getImages();
     }
 
@@ -45,6 +58,47 @@ public class GalleryActivity extends Activity {
         gridView = (GridView) findViewById(R.id.gallery_grid_view);
         gridView.setAdapter(new ImageAdapter(this));
         //Toast.makeText(this,"Loaded", Toast.LENGTH_LONG);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Bitmap image = (Bitmap) parent.getItemAtPosition(position);
+
+                //Get bitmap location
+                Uri imageUri = getImageUri(getApplicationContext(), image, position);
+
+                //Launch Gallery or other image viewer
+                Intent intent = new Intent();
+                //intent.setType("image/*");
+                intent.setDataAndType(imageUri, "image/*");
+                intent.setAction(Intent.ACTION_VIEW);
+                //intent.addCategory(Intent.CATEGORY_OPENABLE);
+                startActivityForResult(intent, 1);
+
+            }
+        });
+        gridView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                Bitmap image = (Bitmap) parent.getItemAtPosition(position);
+                //Get bitmap location
+                Uri imageUri = getImageUri(getApplicationContext(), image, position);
+
+                Toast.makeText(GalleryActivity.this, imageUri.toString(), Toast.LENGTH_LONG).show();
+                return true;
+            }
+        });
+    }
+
+    public Uri getImageUri(Context inContext, Bitmap inImage, int index)
+    {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        inImage.compress(Bitmap.CompressFormat.JPEG, 100, bytes);
+        folder = new File("/sdcard/Documents/HiGallery/.HiGallery");
+        File[] files = folder.listFiles();
+        images = new Bitmap[folder.listFiles().length];
+        int i = index;
+        String path = files[i].getAbsolutePath();
+        return Uri.parse(path);
     }
 
     //For access to right API use the @TargetApi(Number) annoation
